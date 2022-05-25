d6474711fb517608572c824129b023d06811d12c
jesse sheehan <jessetsheehan@gmail.com>
2022-05-25 00:31:33 -0400

    added folder structure, HTML wireframe, connected CSS and JS files, gitignore, and a test js payload object

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//hey Avengers - quick tip: hit (on Mac) option + z to make it so the text wraps, instead of extends out the window - then you won't have to scroll horizontally.
+
+
+//var marvelApiUrl = "http://gateway.marvel.com/v1/public/comics?apikey=ef6e7e667ad6b2c4b1d7956ea5b7281e"
+
+
+
+
+var testPayload = {
+    name: 'spiderman',
+    identity: 'Peter Parker',
+    bio: "Peter Parker was bitten by a radioactive spider, and now fights for New York City, and sometimes... the world",
+    // NOTE: This is how images are used for marvel Api: https://developer.marvel.com/documentation/images 
+    image: "image goes here"
+};
+
+console.log(testPayload);
